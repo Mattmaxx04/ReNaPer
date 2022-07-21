13f36dbaccfebca8ed3b22167b8dab6fe4c0909a
Matias <matiasquiroga.04@gmail.com>
2022-07-21 13:07:53 -0300

    Repository
    Create PersonRepository

---
+++ src/main/java/com/renaper/api/repository/PersonRepository.java
@@ -0,0 +1,14 @@
+package com.renaper.api.repository;
+
+import com.renaper.api.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    Optional<Person> findByNameAndLastname(String name, String lastname);
+    Optional<Person> findPersonByLastnameStartsWith(String lastname);
+    Optional<Person> findPersonById(Long id);
+}
